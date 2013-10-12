0887ac5e513ae1deb8bf14c676e3f4b95ba39772
luizleroy <luizleroy@e5d41feb-c94f-0410-bbc3-d1c7a451970e>
2013-10-12 00:46:27 +0000

    Pause on the development: Java is the path?

---
+++ depot/MainRandom.c
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "boost/random.hpp"
+#include "boost/generator_iterator.hpp"
+using namespace std;
+
+int main() {
+      typedef boost::mt19937 RNGType;
+      RNGType rng;
+      boost::uniform_int<> one_to_six( 1, 4096 );
+      boost::variate_generator< RNGType, boost::uniform_int<> > dice(rng, one_to_six);
+      /*for ( int i = 0; i < 6; i++ ) {
+          int n  = dice();
+          cout << n << endl;
+     }*/
+}
---
+++ depot/Power2.hpp
@@ -0,0 +1,22 @@
+//TODO
+//<class Y = double, class X = vector<double>, class B = long size_t>
+//	static Y f(X x);
+//	static Y f(B b);	
+//	static X size_terpollation(B b) {vector<double> vectorOne(10,5.); return vectorOne;}
+
+template <class T>
+class Power2 {
+    T a, b;
+  public:
+    Power2 (T first, T second)
+      {a=first; b=second;}
+    T getmax ();
+};
+
+template <class T>
+T Power2<T>::getmax ()
+{
+	T retval;
+	retval = a>b? a : b;
+	return retval;
+};
