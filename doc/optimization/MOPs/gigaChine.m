d203570bc5e05a05dc09ec8e3da80a419222b804
luizleroy@gmail.com <luizleroy@gmail.com@e5d41feb-c94f-0410-bbc3-d1c7a451970e>
2014-04-12 12:16:39 +0000

    Base de dados para otimização multiobjetivo.

---
+++ doc/optimization/MOPs/gigaChine.m
@@ -0,0 +1,32 @@
+clear; close all; tic;
+%rng('default');
+
+display('População (cuidado: a análise força bruta cresce com o quadrado do valor!)')
+pop = 1200
+
+func = 'ZDT1';
+f = functionTable(pop, func);
+
+subplot(2,1,1);
+set(0,'DefaultTextFontSize', 20);
+plot(f(:,1),f(:,2),'.');
+
+subplot(2,1,2);
+title('TESTANDO');
+
+maxf1 = max(f(:,1));
+maxf2 = max(f(:,2));
+
+d = f;
+for i=1:pop
+    for j=1:pop % é possível otimizar? Sem fazer o produto cruzado, e sim combinação 2 a 2?
+        if dominioV1(f(i,:),f(j,:))
+            d(j,:) = [maxf1,maxf2];
+        end
+    end
+end
+
+plot(d(:,1),d(:,2),'.r');
+hold on
+plot(maxf1,maxf2,'^r');
+toc;
